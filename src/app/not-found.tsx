3724b852518a1b5ffbd9ee165e7684213fe1bfca
mbardakov <mbardakov@gmail.com>
2025-05-16 15:54:35 -0400

    move demo to subfolder, rename app

---
+++ src/app/not-found.tsx
@@ -0,0 +1,3 @@
+export default function NotFound() {
+    return <h1>Oh, no! Page not found :(</h1>
+}
---
+++ src/app/wife/page.tsx
@@ -0,0 +1,3 @@
+export default function Wife() {
+    return <h1>Hello my wife. I love you</h1>
+}
