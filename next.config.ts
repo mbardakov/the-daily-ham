510aaba8246b486459113e5626a25fcdf1a60b4e
mbardakov <mbardakov@gmail.com>
2025-05-16 20:16:59 -0400

    fixed static image path loading

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/the-daily-ham',
+  // basePath: '/the-daily-ham',
   output: 'export',
   reactStrictMode: true,
+  assetPrefix: "/",
+  images: {
+    loader: 'custom',
+    loaderFile: 'src/app/util/loader.ts',
+  },
   /* config options here */
 };
 
