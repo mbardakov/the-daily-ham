ff1da04f5563ff080464be5d77751b5480c5eff4
mbardakov <mbardakov@gmail.com>
2025-05-17 10:59:10 -0400

    revert changes to width

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,5 @@
 export default function NotFound() {
 	return (
-		<h1 className="font-bold text-4xl my-2 w-3xl">Oh, no! Page not found :(</h1>
+		<h1 className="font-bold text-4xl my-2 max-w-3xl">Oh, no! Page not found :(</h1>
 	)
 }
--- src/app/wife/page.tsx
+++ src/app/wife/page.tsx
@@ -1,7 +1,7 @@
 export default function Wife() {
 	return (
 		<>
-			<h1 className="font-bold text-4xl my-2 w-3xl">Hello my wife. I love you</h1>
+			<h1 className="font-bold text-4xl my-2 max-w-3xl">Hello my wife. I love you</h1>
 		</>
 	)
 }
